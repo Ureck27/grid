f51d84c6383fba1f669fe85e010da6a5fe03094f
rachid aourik <rachidaourik18@gmail.com>
2025-11-28 17:59:28 +0100

    grid

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <div class='container'>
+         <div class="item1"> 1</div>
+         <div class="item2">2</div>
+         <div class="item3">3</div>
+         <div class="item4">4</div>
+         <div class="item5">5</div>
+         <div class="item6">6</div>
+         <div class="item7">7</div>
+         <div class="item8">8</div>
+         <div class="item9">9</div>
+
+     </div>
+    </>
+  )
+}
+
+export default App
